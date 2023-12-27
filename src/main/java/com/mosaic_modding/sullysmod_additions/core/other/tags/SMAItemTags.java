0fab82b8f6e0176e51de1894abf664ab2c67bf52
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2023-12-28 00:32:15 +0100

    Fishermens trap compat

--- src/main/java/com/mosaic_modding/sullysmod_additions/core/other/tags/SMAItemTags.java
+++ src/main/java/com/mosaic_modding/sullysmod_additions/core/other/tags/SMAItemTags.java
@@ -1,6 +1,8 @@
 package com.mosaic_modding.sullysmod_additions.core.other.tags;
 
 import com.teamabnormals.blueprint.core.util.TagUtil;
+import com.uraneptus.fishermens_trap.FishermensTrap;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
@@ -8,4 +10,5 @@ public class SMAItemTags {
 
     //Quark Tags
     public static final TagKey<Item> VERTICAL_SLABS = TagUtil.itemTag("quark", "vertical_slabs");
+    public static final TagKey<Item> GLOW_BERRIES = TagUtil.itemTag(FishermensTrap.MOD_ID, "jei_display_results/minecraft/glow_berries");
 }
--- src/main/java/com/mosaic_modding/sullysmod_additions/data/server/tags/SMAItemTagsProvider.java
+++ src/main/java/com/mosaic_modding/sullysmod_additions/data/server/tags/SMAItemTagsProvider.java
@@ -4,11 +4,14 @@ import com.mosaic_modding.sullysmod_additions.SullysModAdditions;
 import com.mosaic_modding.sullysmod_additions.core.other.tags.SMAItemTags;
 import com.mosaic_modding.sullysmod_additions.core.registry.SMABlocks;
 import com.mosaic_modding.sullysmod_additions.core.registry.SMAItems;
+import com.uraneptus.fishermens_trap.core.other.FTItemTags;
 import com.uraneptus.sullysmod.core.other.tags.SMItemTags;
+import com.uraneptus.sullysmod.core.registry.SMItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -28,5 +31,8 @@ public class SMAItemTagsProvider extends ItemTagsProvider {
         tag(SMItemTags.COOKED_LANTERNFISH).add(SMAItems.COOKED_LANTERNFISH_SLICE.get());
 
         SMABlocks.VERTICAL_SLABS.forEach((block, parent) -> tag(SMAItemTags.VERTICAL_SLABS).add(block.get().asItem()));
+
+        tag(FTItemTags.FISH_BAITS).add(Items.GLOW_BERRIES);
+        tag(SMAItemTags.GLOW_BERRIES).add(SMItems.LANTERNFISH.get());
     }
 }
