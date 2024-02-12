813d4c3c6e91a1e2cee4270ab713fedb42473f49
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2024-02-12 19:47:32 +0100

    finish copperative compat

--- src/main/java/com/mosaic_modding/sullysmod_additions/core/integration/CopperativeCompat.java
+++ src/main/java/com/mosaic_modding/sullysmod_additions/core/integration/CopperativeCompat.java
@@ -1,13 +1,20 @@
 package com.mosaic_modding.sullysmod_additions.core.integration;
 
+import com.google.common.collect.BiMap;
+import com.teamabnormals.blueprint.core.util.BlockUtil;
 import com.uraneptus.sullysmod.common.blocks.WeatheringCopperButtonBlock;
 import com.uraneptus.sullysmod.core.registry.SMBlocks;
 import galena.copperative.content.event.CommonEvents;
 import galena.copperative.content.item.PatinaItem;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
@@ -15,6 +22,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.WeatheringCopper;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
@@ -22,6 +30,8 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.ToolActions;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.level.BlockEvent;
+
+import java.util.Optional;
 
 public class CopperativeCompat {
     private static boolean isActive = false;
@@ -32,35 +42,50 @@ public class CopperativeCompat {
 
     public static void handleAxeScrape(PlayerInteractEvent.RightClickBlock event) {
         Level level = event.getLevel();
-        Block block = level.getBlockState(event.getPos()).getBlock();
+        BlockState blockState = level.getBlockState(event.getPos());
+        Block block = blockState.getBlock();
         ItemStack itemStack = event.getItemStack();
         BlockHitResult hitResult = event.getHitVec();
         Player player = event.getEntity();
+        BlockPos pos = event.getPos();
 
         if (!isActive) return;
-        if (player == null) return;
+        if (player == null ) return;
         if (block instanceof WeatheringCopperButtonBlock) {
-            if (player.isCreative()) {
-                if (level instanceof ServerLevel serverLevel && itemStack.getItem() instanceof AxeItem && WeatheringCopperButtonBlock.getPrevious(block.defaultBlockState()).isPresent()) {
-                    var dropPos = hitResult.getBlockPos();
-                    var clickedFace = hitResult.getDirection();
-                    var lootTable = serverLevel.getServer().getLootData().getLootTable(CommonEvents.PATINA_LOOT_TABLE);
-                    var lootContext = new LootParams.Builder(serverLevel)
-                            .withParameter(LootContextParams.BLOCK_STATE, block.defaultBlockState())
-                            .withParameter(LootContextParams.ORIGIN, hitResult.getLocation())
-                            .withParameter(LootContextParams.TOOL, itemStack)
-                            .withParameter(LootContextParams.THIS_ENTITY, player)
-                            .create(LootContextParamSets.BLOCK);
+            if (level instanceof ServerLevel serverLevel && itemStack.getItem() instanceof AxeItem && WeatheringCopperButtonBlock.getPrevious(blockState).isPresent()) {
+                var dropPos = hitResult.getBlockPos();
+                var clickedFace = hitResult.getDirection();
+                var lootTable = serverLevel.getServer().getLootData().getLootTable(CommonEvents.PATINA_LOOT_TABLE);
+                var lootContext = new LootParams.Builder(serverLevel)
+                        .withParameter(LootContextParams.BLOCK_STATE, blockState)
+                        .withParameter(LootContextParams.ORIGIN, hitResult.getLocation())
+                        .withParameter(LootContextParams.TOOL, itemStack)
+                        .withParameter(LootContextParams.THIS_ENTITY, player)
+                        .create(LootContextParamSets.BLOCK);
 
-                    lootTable.getRandomItems(lootContext).forEach(stack ->
-                            Block.popResourceFromFace(serverLevel, dropPos, clickedFace, stack)
-                    );
-                }
+                lootTable.getRandomItems(lootContext).forEach(stack ->
+                        Block.popResourceFromFace(serverLevel, dropPos, clickedFace, stack)
+                );
             }
             //Can't continue until this is static
-            if (itemStack.getItem() instanceof PatinaItem && WeatheringCopperButtonBlock.getNext(block.defaultBlockState()).isPresent())
+            if (player.isShiftKeyDown() && itemStack.getItem() instanceof PatinaItem && getNextBlock(blockState).isPresent()) {
+                BlockState nextBlock = getNextBlock(blockState).get();
+                level.setBlock(pos, BlockUtil.transferAllBlockStates(blockState, nextBlock), 11);
+                player.swing(event.getHand());
+                level.playSound(player, pos, SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F);
+                level.levelEvent(player, 3005, pos, 0);
+
+                if (!player.isCreative()) {
+                    itemStack.shrink(1);
+                }
+                if (player instanceof ServerPlayer) {
+                    CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, pos, itemStack);
+                }
+            }
         }
+    }
 
-
+    public static Optional<BlockState> getNextBlock(BlockState state) {
+        return Optional.ofNullable((Block)((BiMap<?, ?>)WeatheringCopperButtonBlock.NEXT_BY_BLOCK.get()).get(state.getBlock())).map((block) -> block.withPropertiesOf(state));
     }
 }
